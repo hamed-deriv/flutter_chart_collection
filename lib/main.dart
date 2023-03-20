500793a2163e98f94f002fe5707e5631a8e80163
hamed-deriv <57184669+hamed-deriv@users.noreply.github.com>
2023-03-20 13:23:03 +0800

    add basic pie chart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_chart_collection/pie_chart.dart';
+
 void main() => runApp(const App());
 
 /// The main app widget.
@@ -11,6 +13,34 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) => MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(primarySwatch: Colors.blueGrey),
-        home: const Center(child: Text('Hello World!')),
+        home: const Center(
+          child: PieChart(
+            data: <double>[20, 15, 10, 8, 7, 6, 5, 4, 3, 2],
+            colors: <Color>[
+              Colors.red,
+              Colors.green,
+              Colors.blue,
+              Colors.orange,
+              Colors.purple,
+              Colors.yellow,
+              Colors.teal,
+              Colors.brown,
+              Colors.pink,
+              Colors.grey,
+            ],
+            labels: <String>[
+              'Red',
+              'Green',
+              'Blue',
+              'Orange',
+              'Purple',
+              'Yellow',
+              'Teal',
+              'Brown',
+              'Pink',
+              'Grey',
+            ],
+          ),
+        ),
       );
 }
---
+++ lib/pie_chart.dart
@@ -0,0 +1,146 @@
+import 'dart:math' as math;
+
+import 'package:flutter/material.dart';
+
+/// Pie chart widget.
+class PieChart extends StatefulWidget {
+  /// Initializes a new instance of the [PieChart] class.
+  const PieChart({
+    required this.data,
+    required this.colors,
+    required this.labels,
+  });
+
+  /// The data to be displayed in the pie chart.
+  final List<double> data;
+
+  /// The colors to be used for each data item.
+  final List<Color> colors;
+
+  /// The labels to be used for each data item.
+  final List<String> labels;
+
+  @override
+  _PieChartState createState() => _PieChartState();
+}
+
+class _PieChartState extends State<PieChart> {
+  int? _selectedIndex;
+
+  @override
+  Widget build(BuildContext context) => Container(
+        color: Colors.white,
+        child: SizedBox(
+          width: 400,
+          height: 400,
+          child: Row(
+            children: <Widget>[
+              SizedBox(
+                width: 200,
+                height: 200,
+                child: CustomPaint(
+                  painter: PieChartPainter(
+                    data: widget.data,
+                    colors: widget.colors,
+                    selectedIndex: _selectedIndex,
+                    onItemSelected: (int index) {
+                      setState(() {
+                        _selectedIndex = index;
+                      });
+                    },
+                  ),
+                ),
+              ),
+              const SizedBox(width: 16),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: List<Widget>.generate(
+                    widget.labels.length,
+                    (int index) => Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 4),
+                      child: GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            _selectedIndex = index;
+                          });
+                        },
+                        child: Row(
+                          children: <Widget>[
+                            Container(
+                              width: 10,
+                              height: 10,
+                              color: widget.colors[index],
+                            ),
+                            const SizedBox(width: 4),
+                            Text(
+                              ' ${widget.labels[index]} (${widget.data[index] / 1}%)',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodyText1!
+                                  .copyWith(
+                                    fontWeight: _selectedIndex == index
+                                        ? FontWeight.bold
+                                        : FontWeight.normal,
+                                  ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+}
+
+/// The custom painter for the pie chart.
+class PieChartPainter extends CustomPainter {
+  /// Initializes a new instance of the [PieChartPainter] class.
+  PieChartPainter({
+    required this.data,
+    required this.colors,
+    required this.onItemSelected,
+    this.selectedIndex,
+  });
+
+  /// The data to be displayed in the pie chart.
+  final List<double> data;
+
+  /// The colors to be used for each data item.
+  final List<Color> colors;
+
+  /// The callback function to be called when a legend item is tapped.
+  final Function(int) onItemSelected;
+
+  /// The index of the selected item.
+  final int? selectedIndex;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final double total =
+        data.reduce((double value, double element) => value + element);
+    double startAngle = -math.pi / 2;
+    double endAngle = 0;
+
+    final Rect rect = Rect.fromLTWH(0, 0, size.width, size.height);
+    canvas.clipRect(rect);
+
+    for (int i = 0; i < data.length; i++) {
+      final Paint paint = Paint()
+        ..color = colors[i].withOpacity(selectedIndex == i ? 0.5 : 1);
+
+      endAngle = startAngle + (data[i] / total) * 2 * math.pi;
+      canvas.drawArc(rect, startAngle, endAngle - startAngle, true, paint);
+      startAngle = endAngle;
+    }
+  }
+
+  @override
+  bool shouldRepaint(PieChartPainter oldDelegate) => true;
+}
